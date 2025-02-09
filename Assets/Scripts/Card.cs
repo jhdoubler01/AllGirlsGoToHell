55896b4eb63d47e2bed9b3ff66eaf22d50f7d119
Julia H <jhdoubler01@gmail.com>
2025-02-09 14:07:03 -0500

    testing out stuff

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CardSystem
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+
+    public class Card : ScriptableObject
+    {
+
+        public string cardName;
+        public CardType cardType;
+        public int damageAmt;
+        //add more variables later for more card types
+
+        public enum CardType
+        {
+            Neutral,
+            Verbal,
+            Physical,
+            Quality,
+            Gifts,
+            Service
+        }
+    }
+}
