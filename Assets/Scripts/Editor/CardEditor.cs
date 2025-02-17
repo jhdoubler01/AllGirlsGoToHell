4e7f39306338cb3fa7f9bab0aad8e1f5fa54d9a0
Julia H <jhdoubler01@gmail.com>
2025-02-17 01:18:57 -0500

    messing around with custom inspector

--- Assets/Scripts/Editor/CardEditor.cs
+++ Assets/Scripts/Editor/CardEditor.cs
@@ -3,14 +3,17 @@ using AGGtH.Runtime.Card;
 using AGGtH.Runtime.Enums;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
 
 namespace AGGtH.Editor
-{   
+{
     [CanEditMultipleObjects]
     [CustomEditor(typeof(Card))]
 
     public class CardEditor : UnityEditor.Editor
     {
+        public VisualTreeAsset m_customCardInspector;
         private Card card;
 
         private SerializedProperty m_cardName;
@@ -29,46 +32,65 @@ namespace AGGtH.Editor
         private SerializedProperty m_drawCardAmt;
         private SerializedProperty m_energyGainAmt;
 
-
-        private void OnEnable()
+        public override VisualElement CreateInspectorGUI()
         {
-            card = target as Card;
-            m_cardName = serializedObject.FindProperty("cardName");
+            // Create a new VisualElement to be the root of our Inspector UI.
+            VisualElement myInspector = new VisualElement();
 
-            m_cardLoveLanguageType = serializedObject.FindProperty("cardLoveLanguageType");
-            m_cardActionType = serializedObject.FindProperty("cardActionType");
-            m_actionTargetType = serializedObject.FindProperty("actionTargetType");
-            m_buffType = serializedObject.FindProperty("buffType");
-            m_debuffType = serializedObject.FindProperty("debuffType");
+            // Add a simple label.
+            myInspector.Add(new Label("This is a custom Inspector"));
+            // Load the UXML file.
+            m_customCardInspector = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UIDocuments/CardEditorUIDoc.uxml");
+            // Instantiate the UXML.
+            myInspector = m_customCardInspector.Instantiate();
 
-            m_damageAmt = serializedObject.FindProperty("damageAmt");
-            m_healAmt = serializedObject.FindProperty("healAmt");
-            m_blockAmt = serializedObject.FindProperty("blockAmt");
-            m_drawCardAmt = serializedObject.FindProperty("drawCardAmt");
-            m_energyGainAmt = serializedObject.FindProperty("energyGainAmt");
-
+            // Return the finished Inspector UI.
+            return myInspector;
         }
-        public override void OnInspectorGUI()
-        {
-            //base.OnInspectorGUI();
-            m_cardName.stringValue = (string)EditorGUILayout.TextField("Card Name", m_cardName.stringValue);
 
-            m_cardLoveLanguageType.enumValueIndex = (int)(CardLoveLanguageType)EditorGUILayout.EnumPopup("Love Language Type", (CardLoveLanguageType)m_cardLoveLanguageType.enumValueIndex);
-            m_cardActionType.enumValueIndex = (int)(CardActionType)EditorGUILayout.EnumPopup("Action Type", (CardActionType)m_cardActionType.enumValueIndex);
-            m_actionTargetType.enumValueIndex = (int)(ActionTargetType)EditorGUILayout.EnumPopup("Target Type", (ActionTargetType)m_actionTargetType.enumValueIndex);
-            m_buffType.enumValueIndex = (int)(BuffType)EditorGUILayout.EnumPopup("Buff Type", (BuffType)m_buffType.enumValueIndex);
-            m_debuffType.enumValueIndex = (int)(DebuffType)EditorGUILayout.EnumPopup("Debuff Type", (DebuffType)m_debuffType.enumValueIndex);
+        //    private void OnEnable()
+        //    {
+        //        card = target as Card;
+        //        m_cardName = serializedObject.FindProperty("cardName");
+
+        //        m_cardLoveLanguageType = serializedObject.FindProperty("cardLoveLanguageType");
+        //        m_cardActionType = serializedObject.FindProperty("cardActionType");
+        //        m_actionTargetType = serializedObject.FindProperty("actionTargetType");
+        //        m_buffType = serializedObject.FindProperty("buffType");
+        //        m_debuffType = serializedObject.FindProperty("debuffType");
+
+        //        m_damageAmt = serializedObject.FindProperty("damageAmt");
+        //        m_healAmt = serializedObject.FindProperty("healAmt");
+        //        m_blockAmt = serializedObject.FindProperty("blockAmt");
+        //        m_drawCardAmt = serializedObject.FindProperty("drawCardAmt");
+        //        m_energyGainAmt = serializedObject.FindProperty("energyGainAmt");
+
+        //    }
+        //    public override void OnInspectorGUI()
+        //    {
+        //        //base.OnInspectorGUI();
+        //        serializedObject.Update();
+        //        EditorGUI.BeginChangeCheck();
+
+        //        m_cardName.stringValue = (string)EditorGUILayout.TextField("Card Name", m_cardName.stringValue);
+
+        //        m_cardActionType.enumValueFlag = (int)(CardActionType)EditorGUILayout.EnumFlagsField("Action Type", (CardActionType)m_cardActionType.enumValueIndex);
+
+        //        m_cardLoveLanguageType.enumValueIndex = (int)(CardLoveLanguageType)EditorGUILayout.EnumPopup("Love Language Type", (CardLoveLanguageType)m_cardLoveLanguageType.enumValueIndex);
+        //        m_actionTargetType.enumValueIndex = (int)(ActionTargetType)EditorGUILayout.EnumPopup("Target Type", (ActionTargetType)m_actionTargetType.enumValueIndex);
+        //        m_buffType.enumValueIndex = (int)(BuffType)EditorGUILayout.EnumPopup("Buff Type", (BuffType)m_buffType.enumValueIndex);
+        //        m_debuffType.enumValueIndex = (int)(DebuffType)EditorGUILayout.EnumPopup("Debuff Type", (DebuffType)m_debuffType.enumValueIndex);
 
 
+        //        //if (m_cardActionType.enumValueIndex is (int)CardActionType.Attack)
+        //        //{
+        //        //    m_damageAmt.intValue = EditorGUILayout.IntField("Damage Amount", m_damageAmt.intValue);
 
-            if (card.cardActionType is CardActionType.Attack)
-            {
-                m_damageAmt.intValue = EditorGUILayout.IntField("Damage Amount", m_damageAmt.intValue);
+        //        //}
+        //        EditorGUI.EndChangeCheck();
+        //        serializedObject.ApplyModifiedProperties();
 
-            }
-
-
-        }
+        //    }
     }
 }
 
--- Assets/Scripts/Runtime/Enums/CardActionType.cs
+++ Assets/Scripts/Runtime/Enums/CardActionType.cs
@@ -1,14 +1,17 @@
+using System;
 namespace AGGtH.Runtime.Enums
 {
+    [Flags]
     public enum CardActionType
     {
-        Attack,
-        Heal,
-        Block,
-        Buff, //positive effect (boosts stats)
-        Debuff, //negative effect (lowers stats)
-        DrawCard,
-        GainEnergy,
-        Exhaust
+        None = 0,
+        Attack = 1 << 0,
+        Heal = 1 << 1,
+        Block = 1 << 2,
+        Buff = 1 << 3, //positive effect (boosts stats)
+        Debuff = 1 << 4, //negative effect (lowers stats)
+        DrawCard = 1 << 5,
+        GainEnergy = 1 << 6,
+        Exhaust = 1 << 7,
     }
 }
