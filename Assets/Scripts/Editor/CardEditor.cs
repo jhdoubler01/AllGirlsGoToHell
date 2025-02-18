f22024513f4e9e9ab3ada4b1e018f1012850aa2c
Julia H <jhdoubler01@gmail.com>
2025-02-17 22:39:34 -0500

    ahhhhh

--- Assets/Scripts/Editor/CardEditor.cs
+++ Assets/Scripts/Editor/CardEditor.cs
@@ -1,36 +1,36 @@
-
-using AGGtH.Runtime.Card;
-using AGGtH.Runtime.Enums;
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
+using AGGtH.Runtime.Card;
+using AGGtH.Runtime.Enums;
 
 namespace AGGtH.Editor
 {
     [CanEditMultipleObjects]
-    [CustomEditor(typeof(Card))]
+    [CustomEditor(typeof(CardData))]
 
     public class CardEditor : UnityEditor.Editor
     {
-        public VisualTreeAsset m_customCardInspector;
-        private Card card;
+        private static VisualTreeAsset m_customCardInspector;
+        private SerializedObject m_serializedObject;
+        
+        private const string CardDataDefaultPath = "Assets/Data/CardData/";
 
-        private SerializedProperty m_cardName;
+        #region Cache Card Data
+        private static CardData cachedCardData { get; set; }
+        private List<CardData> allCardList { get; set; }
+        private CardData selectedCardData { get; set; }
+        private string cardID { get; set; }
+        private string cardName { get; set; }
+        private int energyCost { get; set; }
+        private Sprite cardSprite { get; set; }
+        private bool usableWithoutTarget { get; set; }
+        private bool exhaustAfterPlay { get; set; }
 
-        //enums
-        private SerializedProperty m_cardLoveLanguageType;
-        private SerializedProperty m_cardActionType;
-        private SerializedProperty m_actionTargetType;
-        private SerializedProperty m_buffType;
-        private SerializedProperty m_debuffType;
+        #endregion
 
-        //ints
-        private SerializedProperty m_damageAmt;
-        private SerializedProperty m_healAmt;
-        private SerializedProperty m_blockAmt;
-        private SerializedProperty m_drawCardAmt;
-        private SerializedProperty m_energyGainAmt;
 
         public override VisualElement CreateInspectorGUI()
         {
@@ -48,49 +48,49 @@ namespace AGGtH.Editor
             return myInspector;
         }
 
-        //    private void OnEnable()
-        //    {
-        //        card = target as Card;
-        //        m_cardName = serializedObject.FindProperty("cardName");
+        //private void OnEnable()
+        //{
+        //    card = target as Card;
+        //    m_cardName = serializedObject.FindProperty("cardName");
 
-        //        m_cardLoveLanguageType = serializedObject.FindProperty("cardLoveLanguageType");
-        //        m_cardActionType = serializedObject.FindProperty("cardActionType");
-        //        m_actionTargetType = serializedObject.FindProperty("actionTargetType");
-        //        m_buffType = serializedObject.FindProperty("buffType");
-        //        m_debuffType = serializedObject.FindProperty("debuffType");
+        //    m_cardLoveLanguageType = serializedObject.FindProperty("cardLoveLanguageType");
+        //    m_cardActionType = serializedObject.FindProperty("cardActionType");
+        //    m_actionTargetType = serializedObject.FindProperty("actionTargetType");
+        //    m_buffType = serializedObject.FindProperty("buffType");
+        //    m_debuffType = serializedObject.FindProperty("debuffType");
 
-        //        m_damageAmt = serializedObject.FindProperty("damageAmt");
-        //        m_healAmt = serializedObject.FindProperty("healAmt");
-        //        m_blockAmt = serializedObject.FindProperty("blockAmt");
-        //        m_drawCardAmt = serializedObject.FindProperty("drawCardAmt");
-        //        m_energyGainAmt = serializedObject.FindProperty("energyGainAmt");
+        //    m_damageAmt = serializedObject.FindProperty("damageAmt");
+        //    m_healAmt = serializedObject.FindProperty("healAmt");
+        //    m_blockAmt = serializedObject.FindProperty("blockAmt");
+        //    m_drawCardAmt = serializedObject.FindProperty("drawCardAmt");
+        //    m_energyGainAmt = serializedObject.FindProperty("energyGainAmt");
 
-        //    }
-        //    public override void OnInspectorGUI()
-        //    {
-        //        //base.OnInspectorGUI();
-        //        serializedObject.Update();
-        //        EditorGUI.BeginChangeCheck();
+        //}
+        //public override void OnInspectorGUI()
+        //{
+        //    //base.OnInspectorGUI();
+        //    serializedObject.Update();
+        //    EditorGUI.BeginChangeCheck();
 
-        //        m_cardName.stringValue = (string)EditorGUILayout.TextField("Card Name", m_cardName.stringValue);
+        //    m_cardName.stringValue = (string)EditorGUILayout.TextField("Card Name", m_cardName.stringValue);
 
-        //        m_cardActionType.enumValueFlag = (int)(CardActionType)EditorGUILayout.EnumFlagsField("Action Type", (CardActionType)m_cardActionType.enumValueIndex);
+        //    m_cardActionType.enumValueFlag = (int)(CardActionType)EditorGUILayout.EnumFlagsField("Action Type", (CardActionType)m_cardActionType.enumValueIndex);
 
-        //        m_cardLoveLanguageType.enumValueIndex = (int)(CardLoveLanguageType)EditorGUILayout.EnumPopup("Love Language Type", (CardLoveLanguageType)m_cardLoveLanguageType.enumValueIndex);
-        //        m_actionTargetType.enumValueIndex = (int)(ActionTargetType)EditorGUILayout.EnumPopup("Target Type", (ActionTargetType)m_actionTargetType.enumValueIndex);
-        //        m_buffType.enumValueIndex = (int)(BuffType)EditorGUILayout.EnumPopup("Buff Type", (BuffType)m_buffType.enumValueIndex);
-        //        m_debuffType.enumValueIndex = (int)(DebuffType)EditorGUILayout.EnumPopup("Debuff Type", (DebuffType)m_debuffType.enumValueIndex);
+        //    m_cardLoveLanguageType.enumValueIndex = (int)(CardLoveLanguageType)EditorGUILayout.EnumPopup("Love Language Type", (CardLoveLanguageType)m_cardLoveLanguageType.enumValueIndex);
+        //    m_actionTargetType.enumValueIndex = (int)(ActionTargetType)EditorGUILayout.EnumPopup("Target Type", (ActionTargetType)m_actionTargetType.enumValueIndex);
+        //    m_buffType.enumValueIndex = (int)(BuffType)EditorGUILayout.EnumPopup("Buff Type", (BuffType)m_buffType.enumValueIndex);
+        //    m_debuffType.enumValueIndex = (int)(DebuffType)EditorGUILayout.EnumPopup("Debuff Type", (DebuffType)m_debuffType.enumValueIndex);
 
 
-        //        //if (m_cardActionType.enumValueIndex is (int)CardActionType.Attack)
-        //        //{
-        //        //    m_damageAmt.intValue = EditorGUILayout.IntField("Damage Amount", m_damageAmt.intValue);
+        //    //if (m_cardActionType.enumValueIndex is (int)CardActionType.Attack)
+        //    //{
+        //    //    m_damageAmt.intValue = EditorGUILayout.IntField("Damage Amount", m_damageAmt.intValue);
 
-        //        //}
-        //        EditorGUI.EndChangeCheck();
-        //        serializedObject.ApplyModifiedProperties();
+        //    //}
+        //    EditorGUI.EndChangeCheck();
+        //    serializedObject.ApplyModifiedProperties();
 
-        //    }
+        //}
     }
 }
 
