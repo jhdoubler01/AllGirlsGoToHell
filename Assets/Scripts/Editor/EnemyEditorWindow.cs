3b6028090c6a7b1982947ba3a2d97a604cb0fe95
elizapearl01 <146670154+elizapearl01@users.noreply.github.com>
2025-02-23 17:21:14 -0500

    Hope this works

--- Assets/Scripts/Editor/EnemyEditorWindow.cs
+++ Assets/Scripts/Editor/EnemyEditorWindow.cs
@@ -1,19 +1,103 @@
 using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using AGGtH.Runtime.Card;
+using AGGtH.Runtime.Enums;
 
 namespace AGGtH.Editor
 {
-    public class EnemyEditoryWindow : MonoBehaviour
+    public class EnemyEditorWindow : EditorWindow
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private string id;
+        private string cardName;
+        private int healthAmt;
+        private Sprite enemySprite;
+        private EnemyLoveLanguageType enemyLoveLanguageType;
+
+
+        [MenuItem("Window/Enemy Creator")]
+        public static void ShowWindow()
         {
-        
+            GetWindow<EnemyEditorWindow>("Enemy Creator");
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnGUI()
         {
-        
+            GUILayout.Label("Create a New Enemy", EditorStyles.boldLabel);
+
+            id = EditorGUILayout.TextField("ID", id);
+            enemyName = EditorGUILayout.TextField("Enemy Name", enemyName);
+            healthAmt = EditorGUILayout.IntField("Health", healthAmt);
+            enemySprite = (Sprite)EditorGUILayout.ObjectField("Enemy Sprite", enemySprite, typeof(Sprite), false);
+            enemyLoveLanguageType = (EnemuLoveLanguageType)EditorGUILayout.EnumPopup("Enemy Love Language Type", enemyLoveLanguageType);
+ 
+/*             GUILayout.Label("Card Action Type", EditorStyles.boldLabel);
+
+            if (GUILayout.Button("Add Action"))
+            {
+                cardActionDataList.Add(new CardActionData());
+            }
+
+            for (int i = 0; i < cardActionDataList.Count; i++)
+            {
+                if (cardActionDataList[i] == null)
+                {
+                    cardActionDataList[i] = new CardActionData();
+                }
+
+                GUILayout.BeginHorizontal();
+                cardActionDataList[i] = new CardActionData()
+                {
+                    CardActionType = (CardActionType)EditorGUILayout.EnumPopup("Action Type", cardActionDataList[i].CardActionType),
+                    DamageAmt = EditorGUILayout.IntField("Damage Amount", cardActionDataList[i].DamageAmt),
+                    HealAmt = EditorGUILayout.IntField("Heal Amount", cardActionDataList[i].HealAmt),
+                    BlockAmt = EditorGUILayout.IntField("Block", cardActionDataList[i].BlockAmt),
+                };
+
+                if (GUILayout.Button("Remove"))
+                {
+                    cardActionDataList.RemoveAt(i);
+                }
+                GUILayout.EndHorizontal();
+            } */
+
+            if (GUILayout.Button("Create Enemy"))
+            {
+                CreateEnemy();
+            }
+        }
+
+        private void CreateEnemy()
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                Debug.LogError("Card ID cannot be empty.");
+                return;
+            }
+
+            string path = "Assets/Data/EnemyData/" + id + ".asset";
+
+            if (AssetDatabase.LoadAssetAtPath<EnemyData>(path) != null)
+            {
+                Debug.LogError("Enemy with this ID already exists.");
+                return;
+            }
+
+            EnemyData newEnemy = ScriptableObject.CreateInstance<EnemyData>();
+            newEnemy.Id = id;
+            newEnemy.EnemyName = enemyName;
+            newEnemy.HealthAmt = healthAmt;
+            newEnemy.EnemySprite = enemySprite;
+            newEnemy.EnemyLoveLanguageType = enemyLoveLanguageType;
+
+            AssetDatabase.CreateAsset(newEnemy, path);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = newEnemy;
+
+            Debug.Log("Created Enemy: " + id);
         }
     }
 }
