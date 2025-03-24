b8b2548ab9ad83c3269f558c48f5fa1c9427bced
elizapearl01 <146670154+elizapearl01@users.noreply.github.com>
2025-03-24 22:47:38 +0200

    fixed bufftype debufftype errpr

--- Assets/Scripts/Runtime/Characters/CharacterStats.cs
+++ Assets/Scripts/Runtime/Characters/CharacterStats.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using AGGtH.Runtime.Enums;
+using AGGtH.Runtime.UI;
 
 namespace AGGtH.Runtime.Characters
 {
---
+++ Assets/Scripts/Runtime/Enums/BuffDebuffType.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace AGGtH.Runtime.Enums
+{
+    public enum BuffType
+    {
+        None,
+        Strength,
+        Agility,
+        // Add additional buff types as needed
+    }
+
+    public enum DebuffType
+    {
+        None,
+        Weakness,
+        Slowness,
+        // Add additional debuff types as needed
+    }
+}
