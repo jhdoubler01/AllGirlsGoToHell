5f500eb1d9f95e72378b673b849e8df4e5a946c3
elizapearl01 <146670154+elizapearl01@users.noreply.github.com>
2025-03-24 23:18:18 +0200

    to get rid of charactercanvas err

---
+++ Assets/Scripts/Runtime/UI/CharacterCanvas.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using AGGtH.Runtime.Enums;
+
+namespace AGGtH.Runtime
+{
+    public class CharacterCanvas
+    {
+        public void UpdateHealthText(int currentHealth, int maxHealth)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateStatusText()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ApplyStatus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ClearStatus()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
