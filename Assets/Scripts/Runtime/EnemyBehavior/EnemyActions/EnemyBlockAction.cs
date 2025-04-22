79cd288f9338e1badd844d9297565925c03d116a
Julia H <jhdoubler01@gmail.com>
2025-04-22 12:36:42 -0400

    ui edits

--- Assets/Scripts/Runtime/EnemyBehavior/EnemyActions/EnemyBlockAction.cs
+++ Assets/Scripts/Runtime/EnemyBehavior/EnemyActions/EnemyBlockAction.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
+using AGGtH.Runtime.Managers;
+using AGGtH.Runtime.Enums;
 
-namespace AGGtH.Runtime
+namespace AGGtH.Runtime.EnemyBehavior.EnemyActions
 {
-    public class EnemyBlockAction : MonoBehaviour
+    public class EnemyBlockAction : EnemyActionBase
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        public override EnemyActionType ActionType => EnemyActionType.Block;
 
-        // Update is called once per frame
-        void Update()
+        public override void DoAction(EnemyActionParameters actionParameters)
         {
-        
+
+            var newTarget = actionParameters.TargetCharacter
+                ? actionParameters.TargetCharacter
+                : actionParameters.SelfCharacter;
+
+            if (!newTarget) return;
+
+            newTarget.CharacterStats.ApplyStatus(StatusType.Block,
+                Mathf.RoundToInt(actionParameters.Value + actionParameters.SelfCharacter.CharacterStats
+                    .StatusDict[StatusType.Dexterity].StatusValue));
+
+            //if (FxManager != null)
+            //    FxManager.PlayFx(newTarget.transform, FxType.Block);
+
+            //if (AudioManager != null)
+            //    AudioManager.PlayOneShot(AudioActionType.Block);
         }
     }
 }
