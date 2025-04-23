d5dc62ecd627b5c1ff22eb0300b8945b8823aeee
elizapearl01 <146670154+elizapearl01@users.noreply.github.com>
2025-04-23 10:56:01 -0400

    setting up MapManager stuff

--- Assets/Scripts/Runtime/Managers/MapManager.cs
+++ Assets/Scripts/Runtime/Managers/MapManager.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.TileMaps;
 
 namespace AGGtH.Runtime
 {
+    public class MapData{
+        public int width;
+        public int height;
+        public int tiles[]; //length = width*height
+    }
     public class MapManager : MonoBehaviour
     {
         // Start is called once before the first execution of Update after the MonoBehaviour is created
