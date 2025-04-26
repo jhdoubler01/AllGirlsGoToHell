dc2d1d94875250a34a82e0294438d0328aee1b0d
elizapearl01 <146670154+elizapearl01@users.noreply.github.com>
2025-04-26 18:23:40 -0400

    GameData for save vars added

--- Assets/Scripts/Runtime/Data/Save/GameData.cs
+++ Assets/Scripts/Runtime/Data/Save/GameData.cs
@@ -6,6 +6,37 @@ namespace AGGtH.Runtime
 {
     public class GameData
     {
-        
+        //player progress
+        public int currentStageId;
+        public int currentEncounterId;
+        public bool isFinalEncounter;
+
+        //stats
+        public int currentHealth;
+        public int maxHealth;
+        public int currentEnergy;
+        public int maxEnergy;
+        public int currentGold;
+
+        //inventory and cards
+        public List<CardData> currentCardsList;
+        public List<CardData> drawPile;
+        public List<CardData> discardPile;
+        public List<CardData> exhaustPile;
+
+        //settings
+        public float volme;
+        public bool isFullScreen;
+        public string resolution;
+        public List<string> unlockedAchievements;
+        public DateTime lastSaveTime;
+
+        //encounter data
+        public EnemyEncounter currentEnemyEncounter;
+        public List<EnemyBase> currentEnemiesList;
+        public bool canUseCards;
+        public bool canSelectCards;
+        public bool isRandomHand;
+
     }
 }
