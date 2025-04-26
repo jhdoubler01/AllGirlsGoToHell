ee1df69566da727ebe91fbda9a29d7fe3cf0cb03
elizapearl01 <146670154+elizapearl01@users.noreply.github.com>
2025-04-26 18:26:49 -0400

    added data to SaveManager

--- Assets/Scripts/Runtime/Managers/SaveManager.cs
+++ Assets/Scripts/Runtime/Managers/SaveManager.cs
@@ -28,7 +28,28 @@ namespace AGGtH.Runtime
         {
             var gameData = new GameData
             {
-                // Populate game data here
+                currentStageId = GameManager.Instance.PersistentGameplayData.CurrentStageId,
+                currentEncounterId = GameManager.Instance.PersistentGameplayData.CurrentEncounterId,
+                isFinalEncounter = GameManager.Instance.PersistentGameplayData.IsFinalEncounter,
+                currentHealth = GameManager.Instance.PersistentGameplayData.PlayerHealthData.CurrentHealth,
+                maxHealth = GameManager.Instance.PersistentGameplayData.PlayerHealthData.MaxHealth,
+                currentEnergy = GameManager.Instance.PersistentGameplayData.CurrentEnergy,
+                maxEnergy = GameManager.Instance.PersistentGameplayData.MaxEnergy,
+                currentGold = GameManager.Instance.PersistentGameplayData.CurrentGold,
+                currentCardsList = GameManager.Instance.PersistentGameplayData.CurrentCardsList,
+                drawPile = CardCollectionManager.Instance.DrawPile,
+                discardPile = CardCollectionManager.Instance.DiscardPile,
+                exhaustPile = CardCollectionManager.Instance.ExhaustPile,
+                volume = AudioListener.volume,
+                isFullScreen = Screen.fullScreen,
+                resolution = Screen.currentResolution.ToString(),
+                unlockedAchievements = new List<string>(), // Populate as needed
+                lastSaveTime = DateTime.Now,
+                currentEncounter = EncounterManager.Instance.CurrentEncounter,
+                currentEnemiesList = EncounterManager.Instance.CurrentEnemiesList,
+                canUseCards = GameManager.Instance.PersistentGameplayData.CanUseCards,
+                canSelectCards = GameManager.Instance.PersistentGameplayData.CanSelectCards,
+                isRandomHand = GameManager.Instance.PersistentGameplayData.IsRandomHand
             };
 
             string json = JsonUtility.ToJson(gameData, true);
@@ -45,7 +66,26 @@ namespace AGGtH.Runtime
                 string json = File.ReadAllText(saveFilePath);
                 GameData gameData = JsonUtility.FromJson<GameData>(json);
 
-                // Load game data here
+                GameManager.Instance.PersistentGameplayData.CurrentStageId = gameData.currentStageId;
+                GameManager.Instance.PersistentGameplayData.CurrentEncounterId = gameData.currentEncounterId;
+                GameManager.Instance.PersistentGameplayData.IsFinalEncounter = gameData.isFinalEncounter;
+                GameManager.Instance.PersistentGameplayData.PlayerHealthData.CurrentHealth = gameData.currentHealth;
+                GameManager.Instance.PersistentGameplayData.PlayerHealthData.MaxHealth = gameData.maxHealth;
+                GameManager.Instance.PersistentGameplayData.CurrentEnergy = gameData.currentEnergy;
+                GameManager.Instance.PersistentGameplayData.MaxEnergy = gameData.maxEnergy;
+                GameManager.Instance.PersistentGameplayData.CurrentGold = gameData.currentGold;
+                GameManager.Instance.PersistentGameplayData.CurrentCardsList = gameData.currentCardsList;
+                CardCollectionManager.Instance.DrawPile = gameData.drawPile;
+                CardCollectionManager.Instance.DiscardPile = gameData.discardPile;
+                CardCollectionManager.Instance.ExhaustPile = gameData.exhaustPile;
+                AudioListener.volume = gameData.volume;
+                Screen.fullScreen = gameData.isFullScreen;
+                EncounterManager.Instance.CurrentEncounter = gameData.currentEncounter;
+                EncounterManager.Instance.CurrentEnemiesList = gameData.currentEnemiesList;
+                GameManager.Instance.PersistentGameplayData.CanUseCards = gameData.canUseCards;
+                GameManager.Instance.PersistentGameplayData.CanSelectCards = gameData.canSelectCards;
+                GameManager.Instance.PersistentGameplayData.IsRandomHand = gameData.isRandomHand;
+                
                 Debug.Log("Game loaded from " + saveFilePath);
                 return true;
             }
