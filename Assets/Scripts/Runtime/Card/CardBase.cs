a37ff7371e68f14f3d2523a76aa4d8fb3481aab4
Julia H <jhdoubler01@gmail.com>
2025-04-28 00:19:13 -0400

    still trying to make the ui better

--- Assets/Scripts/Runtime/Card/CardBase.cs
+++ Assets/Scripts/Runtime/Card/CardBase.cs
@@ -12,20 +12,20 @@ using AGGtH.Runtime.Characters;
 
 namespace AGGtH.Runtime.Card
 {
-    public class CardBase : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler/*, I2DTooltipTarget, IPointerDownHandler, IPointerUpHandler*/
+    public class CardBase : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         [Header("Card Objects")]
-        [SerializeField] private TMP_Text cardNameText;
-        [SerializeField] private TMP_Text cardDescText;
-        [SerializeField] private TMP_Text energyCostText;
-        [SerializeField] private TMP_Text actionAmtText;
-        [SerializeField] private Image cardImage;
-        [SerializeField] private Image cardTooltipImage;
+        [SerializeField] protected TMP_Text cardNameText;
+        [SerializeField] protected TMP_Text cardDescText;
+        [SerializeField] protected TMP_Text energyCostText;
+        [SerializeField] protected TMP_Text actionAmtText;
+        [SerializeField] protected Image cardImage;
+        [SerializeField] protected Image cardTooltipImage;
 
         private Transform playerHandParent;
-        private bool overValidTarget;
-        private Transform parentAfterDrag;
-        private EnemyBase enemyTarget;
+        //private bool overValidTarget;
+        //private Transform parentAfterDrag;
+        //private EnemyBase enemyTarget;
 
         private int enemyTargetLayer;
 
@@ -43,6 +43,8 @@ namespace AGGtH.Runtime.Card
         protected CardCollectionManager CardCollectionManager => CardCollectionManager.Instance;
         public bool IsExhausted { get; private set;}
         public bool IsSelected = false;
+
+        public event Action<CardBase> OnCardClicked, OnHover;
 
         #endregion
 
@@ -87,77 +89,78 @@ namespace AGGtH.Runtime.Card
         }
         #endregion
 
-        #region Card Drag Handlers
-        public void OnCardSelected()
-        {
-            IsSelected = !IsSelected;
-            EncounterManager.SelectedCard(this, IsSelected);
-        }
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            if (!GameManager.PersistentGameplayData.CanSelectCards) { return; }
+        #region Card Drag Handlers OLD
 
-            PlayableOnBeginDrag();
-        }
-        public void OnDrag(PointerEventData eventData)
-        {
-            transform.position = Input.mousePosition;
-        }
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            PlayableOnEndDrag();
-        }
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            PlayableTriggerEnter(collision);
-        }
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            PlayableTriggerExit(collision);
-        }
-        public virtual void PlayableTriggerEnter(Collider2D collision)
-        {
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Enemies"))
-            {
-                Debug.Log("enemy collision");
-                parentAfterDrag = collision.transform;
-                enemyTarget = collision.gameObject.GetComponent<EnemyBase>();
-                overValidTarget = true;
-            }
-            else if(collision.gameObject.transform == playerHandParent)
-            {
-                overValidTarget = false;
-                parentAfterDrag = playerHandParent;
-            }
-        }
-        public virtual void PlayableTriggerExit(Collider2D collision)
-        {
-            if (overValidTarget)
-            {
-                parentAfterDrag = playerHandParent;
-                overValidTarget = false;
-                enemyTarget = null;
-            }
-        }
-        public virtual void PlayableOnBeginDrag()
-        {
-            CardCollectionManager.HandController.RemoveCardFromHand(this);
-            parentAfterDrag = playerHandParent;
-            transform.SetParent(transform.root);
-            transform.SetAsLastSibling();
-            if (CardData.UsableWithoutTarget) { overValidTarget = true; }
-            Debug.Log(overValidTarget);
-        }
+        //public void OnCardSelected()
+        //{
+        //    IsSelected = !IsSelected;
+        //    EncounterManager.SelectedCard(this, IsSelected);
+        //}
+        //public void OnBeginDrag(PointerEventData eventData)
+        //{
+        //    if (!GameManager.PersistentGameplayData.CanSelectCards) { return; }
 
-        public virtual void PlayableOnEndDrag()
-        {
-            Debug.Log(IsPlayable);
-            transform.SetParent(parentAfterDrag);
+        //    PlayableOnBeginDrag();
+        //}
+        //public void OnDrag(PointerEventData eventData)
+        //{
+        //    transform.position = Input.mousePosition;
+        //}
+        //public void OnEndDrag(PointerEventData eventData)
+        //{
+        //    PlayableOnEndDrag();
+        //}
+        //private void OnTriggerEnter2D(Collider2D collision)
+        //{
+        //    PlayableTriggerEnter(collision);
+        //}
+        //private void OnTriggerExit2D(Collider2D collision)
+        //{
+        //    PlayableTriggerExit(collision);
+        //}
+        //public virtual void PlayableTriggerEnter(Collider2D collision)
+        //{
+        //    if (collision.gameObject.layer == LayerMask.NameToLayer("Enemies"))
+        //    {
+        //        Debug.Log("enemy collision");
+        //        parentAfterDrag = collision.transform;
+        //        enemyTarget = collision.gameObject.GetComponent<EnemyBase>();
+        //        overValidTarget = true;
+        //    }
+        //    else if(collision.gameObject.transform == playerHandParent)
+        //    {
+        //        overValidTarget = false;
+        //        parentAfterDrag = playerHandParent;
+        //    }
+        //}
+        //public virtual void PlayableTriggerExit(Collider2D collision)
+        //{
+        //    if (overValidTarget)
+        //    {
+        //        parentAfterDrag = playerHandParent;
+        //        overValidTarget = false;
+        //        enemyTarget = null;
+        //    }
+        //}
+        //public virtual void PlayableOnBeginDrag()
+        //{
+        //    CardCollectionManager.HandController.RemoveCardFromHand(this);
+        //    parentAfterDrag = playerHandParent;
+        //    transform.SetParent(transform.root);
+        //    transform.SetAsLastSibling();
+        //    if (CardData.UsableWithoutTarget) { overValidTarget = true; }
+        //    Debug.Log(overValidTarget);
+        //}
 
-            if (IsPlayable && overValidTarget && GameManager.PersistentGameplayData.CurrentEnergy >= CardData.EnergyCost) { Use(EncounterManager.Player, enemyTarget, EncounterManager.CurrentEnemiesList, EncounterManager.Player); return; }
+        //public virtual void PlayableOnEndDrag()
+        //{
+        //    Debug.Log(IsPlayable);
+        //    transform.SetParent(parentAfterDrag);
 
-            CardCollectionManager.HandController.AddCardToHand(this);
-        }
+        //    if (IsPlayable && overValidTarget && GameManager.PersistentGameplayData.CurrentEnergy >= CardData.EnergyCost) { Use(EncounterManager.Player, enemyTarget, EncounterManager.CurrentEnemiesList, EncounterManager.Player); return; }
+
+        //    CardCollectionManager.HandController.AddCardToHand(this);
+        //}
 
         #endregion
 
@@ -242,28 +245,31 @@ namespace AGGtH.Runtime.Card
             return CardData.DialogueOptions[rand];
         }
         #endregion
-        //#region Pointer Events
-        //public virtual void OnPointerEnter(PointerEventData eventData)
-        //{
-        //    ShowTooltipInfo();
-        //}
+        #region Pointer Events
+        public virtual void OnPointerEnter(PointerEventData eventData)
+        {
+            //ShowTooltipInfo();
+        }
 
-        //public virtual void OnPointerExit(PointerEventData eventData)
-        //{
-        //    HideTooltipInfo(TooltipManager.Instance);
-        //}
+        public virtual void OnPointerExit(PointerEventData eventData)
+        {
+            //HideTooltipInfo(TooltipManager.Instance);
+        }
+        public virtual void OnPointedClick(PointerEventData eventData)
+        {
+            OnCardClicked?.Invoke(this);
+        }
+        public virtual void OnPointerDown(PointerEventData eventData)
+        {
+            //HideTooltipInfo(TooltipManager.Instance);
+        }
 
-        //public virtual void OnPointerDown(PointerEventData eventData)
-        //{
-        //    HideTooltipInfo(TooltipManager.Instance);
-        //}
-
-        //public virtual void OnPointerUp(PointerEventData eventData)
-        //{
-        //    ShowTooltipInfo();
-        //}
-        //#endregion
-        //#region Tooltip
+        public virtual void OnPointerUp(PointerEventData eventData)
+        {
+            //ShowTooltipInfo();
+        }
+        #endregion
+        #region Tooltip
         //protected virtual void ShowTooltipInfo()
         //{
         //    if (!descriptionRoot) return;
@@ -286,7 +292,7 @@ namespace AGGtH.Runtime.Card
         //{
         //    tooltipManager.HideTooltip();
         //}
-        //#endregion
+        #endregion
         #region Routines
 
         #endregion
